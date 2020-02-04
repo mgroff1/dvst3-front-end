867cea52c48c7f6f568378fb62ae7c6ef3e95bd9
Krystal Guzman <kryguzman@gmail.com>
2020-02-04 13:23:06 -0800

    connected login and signup

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -10,7 +10,7 @@ import {Button, Col, Row, Label} from 'reactstrap';
 
 
 
-const SubmitForm = ({ values, errors, touched, status }) => {
+const SubmitForm = ({ history, values, errors, touched, status }) => {
   const [members, setMembers] = useState([]);
   useEffect(() => {
     console.log("status has changed", status);
@@ -22,16 +22,11 @@ const SubmitForm = ({ values, errors, touched, status }) => {
       <Form>
         <h2>Log In Form</h2>
           <Col>
-        <Label htmlFor="name">Name: </Label>
-        <Field id="name" type="text" name="name" placeholder="Name" />
-        {touched.name && errors.name && 
-          (<p className="errors">{errors.name})</p>)}
+        <Label htmlFor="username">Username: </Label>
+        <Field id="username" type="text" name="username" placeholder="Userame" />
+        {touched.username && errors.username && 
+          (<p className="errors">{errors.username})</p>)}
 
-        </Col><Col>
-        <Label htmlFor="email">Email: </Label>
-        <Field id="email" type="email" name="email" placeholder="Email" />
-        {touched.email && errors.email && <p className="errors">{errors.email}</p>}
-        
         </Col><Col>
         <Label htmlFor="password">Password: </Label>
         <Field id="password" type="password" name="password" placeholder="Password" />
@@ -45,53 +40,41 @@ const SubmitForm = ({ values, errors, touched, status }) => {
 
       {/* delete for backend log in authenticate goes here?*/}
 </Row>
-      {members.map(member => (
-        <ul key={member.id}>
-          <li>Name: {member.name}</li>
-          <li>Email: {member.email}</li>
-          <li>Password: {member.password}</li>
-        </ul>
-      ))}
     </div>
   );
 };
 
 const FormikLogInForm = withFormik({
-  mapPropsToValues({ name, email, password }) {
+  mapPropsToValues({ username, password }) {
     return {
-      name: name || "",
-      email: email || "",
+      username: username || "",
       password: password || "",
     };
   },
   validationSchema: Yup.object().shape({
-    name: Yup.string()
-    .required("Name is required.")
+    username: Yup.string()
+    .required("Username is required.")
     .min(2,'Choose a longer name!')
     .max(50, 'Choose a shorter name!'),
-    email: Yup.string()
-    .required("Email is required.")
-    .email('Email is not valid!'),
     password: Yup.string()
     .required("Password is required.")
-    .min(6,'Choose a stronger password!')
+    .min(4,'Choose a stronger password!')
     .max(30, 'Choose a shorter password!')
 }),
-  handleSubmit(values, { resetForm, setErrors, setStatus }, ) {
+  handleSubmit(values, { resetForm, setErrors, setStatus, props }, ) {
     console.log("submitting", values);
-    // change to filter through logins
-    if (values.email === "tester@test.com") {
-        setErrors({ email: "That email is already taken" });
-    }else{
+   
     axios
-      .post("https://reqres.in/api/users/", values)
+      .post("https://dvst3-be.herokuapp.com/api/users/login", values)
       .then(res => {
         console.log("success", res.data);
+        localStorage.setItem('token', res.data.token)
         setStatus(res.data);
         resetForm();
+        props.history.push("/Calculator");
       })
       .catch(err => console.log("Error:", err.response));
-  }
+  
 }
 })(SubmitForm);
 export default FormikLogInForm;
